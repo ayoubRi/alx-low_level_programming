8f60bf80656bed96409ae4ab9ea27ee882c881ef
ayoubRi <aa2477953@gmail.com>
2023-06-19 17:55:37 -0700

    101-quote

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -5,8 +5,10 @@
  * main : entry point
  * Return : always 1 (success)
  * */
- int main(void) 
- {
-    putc(STDOUT_FILENO, "and that piece of art is useful" - Dora Korpar, 2015-10-19/n );
-    return (1);
+ int main(void) ;
+
+ { 
+    putc (STDOUT_FILENO, "and that piece of art is useful\" - Dora Korpar, 2015-10-19/n , 59);
+    Return (1) 
  }
+
