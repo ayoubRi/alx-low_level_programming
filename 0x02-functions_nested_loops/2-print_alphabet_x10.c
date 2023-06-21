1263dbe91a626889162aef95bdb64e0e9ff86c7a
ayoubRi <aa2477953@gmail.com>
2023-06-20 19:18:46 -0700

    2-print_alphabet_x10.c

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -10,11 +10,9 @@ void print_alphabet_x10(void)
 
 	for (i = 0; i < 10; i++)
 	{
-		letter = 'a';
-		while (letter <= 'z')
+		for (letter = 'a'; letter <= 'z'; letter++)
 		{
 			_putchar(letter);
-			letter++;
 		}
 	}
 
