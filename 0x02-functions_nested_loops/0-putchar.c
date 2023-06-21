e1f068e1d20bb25d099d0cdf02120364b7882415
ayoubRi <aa2477953@gmail.com>
2023-06-20 19:02:39 -0700

    0-putchar.c

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+int main(void)
+{
+	print_putchar();
+	_putchar('\n');
+
+	return 0;
+}
+
+void print_putchar(void)
+{
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+}
+
